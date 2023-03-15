6c7482e7ab61a8f6b714d6cb980395127a01a504
Rastsislau <ungaf@ya.ru>
2023-03-15 11:31:56 +0300

    clean boolean checks

--- form_generators.py
+++ form_generators.py
@@ -3,12 +3,12 @@ DISJUNCTION = "\\/"
 
 
 def make_fcnf(table, variables):
-    table = list(filter(lambda x: x[1] is False, table))
+    table = list(filter(lambda x: not x[1], table))
     constituents = []
     for values, _ in table:
         used_variables = []
         for variable, value in zip(variables, values):
-            if value is True:
+            if value:
                 used_variables.append(f"!{variable}")
             else:
                 used_variables.append(f"{variable}")
@@ -21,7 +21,7 @@ def make_numeric_fcnf(table):
         binary_str = ''.join('1' if b else '0' for b in bool_tuple)
         return int(binary_str, 2)
 
-    table = filter(lambda x: x[1] is False, table)
+    table = filter(lambda x: not x[1], table)
     table = map(lambda x: x[0], table)
     table = map(lambda x: tuple_to_int(x), table)
     return f"/\\{tuple(table)}"
@@ -41,12 +41,12 @@ def make_index(table):
 
 
 def make_fdnf(table, variables):
-    table = list(filter(lambda x: x[1] is True, table))
+    table = list(filter(lambda x: x[1], table))
     constituents = []
     for values, _ in table:
         vars2 = []
         for variable, value in zip(variables, values):
-            if value is False:
+            if not value:
                 vars2.append(f"!{variable}")
             else:
                 vars2.append(f"{variable}")
@@ -59,7 +59,7 @@ def make_numeric_fdnf(table):
         binary_str = ''.join('1' if b else '0' for b in bool_tuple)
         return int(binary_str, 2)
 
-    table = filter(lambda x: x[1] is True, table)
+    table = filter(lambda x: x[1], table)
     table = map(lambda x: x[0], table)
     table = map(lambda x: tuple_to_int(x), table)
     return f"\\/{tuple(table)}"
